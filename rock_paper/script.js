a47cbbd60b03508c45eb6895d6c7630914a8b090
shirschfield <79327844+shirschfield@users.noreply.github.com>
2024-04-03 14:41:04 -0400

    Add game

---
+++ rock_paper/script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let number = Math.floor(Math.random() * 3);
+    if (number === 0) {
+        return "Rock";
+    } else if (number === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
