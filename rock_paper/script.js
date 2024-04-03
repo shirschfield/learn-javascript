1e9f3991f66437b4deb1eaf8c028e2e05b1d7a39
shirschfield <79327844+shirschfield@users.noreply.github.com>
2024-04-03 15:00:28 -0400

    Update game

--- rock_paper/script.js
+++ rock_paper/script.js
@@ -10,3 +10,33 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+
+function playRound(playerSelection, computerSelection) {
+    
+    if (playerSelection === !("")){
+        return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase(); 
+     } if (playerSelection === "Rock" + computerSelection === "Scissors") {
+        return "You win! Rock beats scissors";
+    } else if (playerSelection === "Scissors" + computerSelection === "Rock") {
+        return "You lose! Rock beats scissors";
+    }  else if (playerSelection === "Scissors" + computerSelection === "Paper") {
+        return "You win! Scissors beats paper";
+    }  else if (playerSelection === "Paper" + computerSelection === "Scissors") {
+        return "You lose! Scissors beats paper";
+    }  else if (playerSelection === "Rock" + computerSelection === "Paper") {
+        return "You lose! Paper beats rock";
+    }  else if (playerSelection === "Paper" + computerSelection === "Rock") {
+        return "You win! Paper beats rock";
+    }  else {
+        return "It's a tie! You both choose ${playerSelection}";
+  }
+}
+  
+  console.log(playRound(playerSelection, computerSelection));
+
+  
