592131f33b23ec9833efb6ac646e180cbbdb31cb
shirschfield <79327844+shirschfield@users.noreply.github.com>
2024-05-31 18:53:12 -0400

    Set up files more

--- Library/script.js
+++ Library/script.js
@@ -10,21 +10,9 @@ function Book(title, author, pages, read) {
     console.log(`${this.title} by ${this.author}, ${pages} pages, ${read}.`)
   }
 }
-
-function Player(name, marker) {
-    this.name = name;
-    this.marker = marker;
-    this.sayName = function() {
-      console.log(this.name)
-    };
-  }
   
-  const player1 = new Player('steve', 'X');
-  const player2 = new Player('also steve', 'O');
-  player1.sayName(); // logs 'steve'
-  player2.sayName(); // logs 'also steve'
-
-
+  const book1 = new Book('The Truth', 'Yenta', '200', 'not read');
+  book1.info(); 
 
 
 function addBookToLibrary() {
