5062853e77b6a286a9199ca413f831d127b69a26
shirschfield <79327844+shirschfield@users.noreply.github.com>
2024-06-16 19:20:23 -0400

    continue libary project

--- Library/script.js
+++ Library/script.js
@@ -1,4 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
 const myLibrary = [];
+
+
+
+const list = document.querySelector("ul");
+const inputTitle = document.querySelector("#title");
+const inputAuthor = document.querySelector("#author");
+const inputPages = document.querySelector("#pages");
+const inputRead = document.querySelector("#read");
+const addButton = document.querySelector("button");
+
+addButton.addEventListener("click", () => {
+  addBookToLibrary()
+})
+
+
+const input = document.querySelector("input");
+
+addButton.addEventListener("click", () => {
+     const itemText = input.value;
+     input.value = "";
+    
+     const listItem = document.createElement("li");
+     const span = document.createElement("span");
+     const deleteButton = document.createElement("button");
+
+     listItem.appendChild(span);
+     span.textContent = itemText;
+     listItem.appendChild(deleteButton);
+     deleteButton.textContent = "Delete";
+     list.appendChild(listItem);
+
+     deleteButton.addEventListener("click", function(){
+        list.removeChild(listItem);
+     });
+
+     input.focus();
+});
+
 
 function Book(title, author, pages, read) {
   // the constructor...
@@ -16,5 +55,14 @@ function Book(title, author, pages, read) {
 
 
 function addBookToLibrary() {
-  // do stuff here
+  const title = inputTitle.value
+  const author = inputAuthor.value
+  const pages = inputPages.value
+  const read = inputRead.checked ? "read" : "not read"
+
+  const newBook = new Book(title, author, pages, read)
+  myLibrary.push(newBook)
 }
+
+})
+
